a3b1981278c7579f3c69ba8ca59990eba1516b25
greg-is-kub <greg_is_kub@pm.me>
2025-09-14 00:18:46 +0200

    feat (SaveStateManager): added build_save_path + renaming of attributes

--- pkmn_rl_arena/env/save_state.py
+++ pkmn_rl_arena/env/save_state.py
@@ -1,13 +1,11 @@
-from pathlib import Path
 from pkmn_rl_arena import SAVE_PATH
 from pkmn_rl_arena.logging import logger
 
-from .battle_core import BattleCore
+from .battle_core import BattleState, BattleCore
 
 import os
-from typing import List
 
-from regex import re
+import re
 
 
 class SaveStateManager:
@@ -15,17 +13,20 @@ class SaveStateManager:
     Manages emulator save states for quick save/load functionality.
     """
 
-    regex = {
-        "stamp": re.compile(".+_turntype:[[:alpha]]_step:\d+.*"),
-        "file_ext": re.compile(".+\.savestate"),
-    }
+    save_file_ext = ".save_state"
+
+    regex_file_ext = re.compile(f".+\{save_file_ext}")
 
     def __init__(self, battle_core: BattleCore):
-        self.battle_core = battle_core
+        self.core = battle_core
         self.save_dir = SAVE_PATH
         self.save_states = []
         self.state_wild_card = "_turntype:*_step:*.savestate"
         os.makedirs(self.save_dir, exist_ok=True)
+
+    @staticmethod
+    def buid_save_path(name: str, state: BattleState):
+        return f"{name}_turntype:{state.turn.value}_step:{state.step}_id:{state.id}{SaveStateManager.save_file_ext}"
 
     def save_state(self, name: str):
         """
@@ -37,9 +38,9 @@ class SaveStateManager:
                 state = { turntype = Turntype.CREATE_TEAM , episode_steps = 0}
                 final name = boot_state_turntype:Turntype.CREATE_TEAM_step:0.savestate"
         """
-        name += f"_turntype:{self.battle_core.state.turn}_step:{self.battle_core.state.step}"
-        self.save_states.append(name)
-        self.battle_core.save_savestate(name)
+        save_path = SaveStateManager.buid_save_path(name, self.core.state)
+        self.save_states.append(save_path)
+        self.core.save_savestate(save_path)
 
     def load_state(self, name: str | None) -> bool:
         """
@@ -64,20 +65,21 @@ class SaveStateManager:
             if len(self.save_states) == 0:
                 logger.fatal("No save state available to load. Exiting.")
                 exit(1)
-            return self.battle_core.load_savestate(self.save_states[0])
+            return self.core.load_savestate(self.save_states[0])
 
         # case 2
-        if not re.match(SaveStateManager.regex["file_ext"], name):
-            logger.debug(f"Given state without file ext, attempting to load 1st state whose name matches regex {name}.+")
+        if not re.match(SaveStateManager.regex_file_ext, name):
+            logger.debug(
+                f"Given state without file ext, attempting to load 1st state whose name matches regex {name}.+"
+            )
             for save in self.save_states:
                 if re.match(f"{name}.+", save):
-                    found
-                    return self.battle_core.load_savestate(save)
+                    return self.core.load_savestate(save)
             if name not in self.save_states:
                 raise ValueError(f"Save state not found at path : {name} exiting.")
         # case 3
         else:
-            return self.battle_core.load_savestate(name)
+            return self.core.load_savestate(name)
 
     def remove_save_states(self):
         """Delete all save states."""
