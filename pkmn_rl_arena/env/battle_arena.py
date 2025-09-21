13a8c4675be1901bf44ee60677be9d8b995a2976
wissammm <wissamboussella@gmail.com>
2025-09-21 17:23:16 +0200

    [feat] replay buffer directly inside battle_arena to store experience while learning

--- pkmn_rl_arena/env/battle_arena.py
+++ pkmn_rl_arena/env/battle_arena.py
@@ -25,6 +25,26 @@ from pettingzoo.utils import AgentSelector
 from rich.console import Console
 from rich.table import Table
 
+class ReplayBuffer:
+    """A simple replay buffer for storing transitions."""
+    def __init__(self, capacity=10000):
+        self.capacity = capacity
+        self.buffer = []
+        self.position = 0
+
+    def push(self, transition):
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(None)
+        self.buffer[self.position] = transition
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        import random
+        batch = random.sample(self.buffer, batch_size)
+        return batch
+
+    def __len__(self):
+        return len(self.buffer)
 
 class RenderMode(Enum):
     """Enumeration for different rendering mode"""
