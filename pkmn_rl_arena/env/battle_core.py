14bfdebaa853b9aae6039e038e434d99dc9ad284
greg-is-kub <greg_is_kub@pm.me>
2025-09-23 10:07:03 +0200

    chore : deleted redundant BattleState / BattleStateFactory classes

--- pkmn_rl_arena/env/battle_core.py
+++ pkmn_rl_arena/env/battle_core.py
@@ -1,58 +1,14 @@
-import rustboyadvance_py
 from pkmn_rl_arena.paths import PATHS
 from pkmn_rl_arena import log
 import pkmn_rl_arena.data.parser
 import pkmn_rl_arena.data.pokemon_data
 from pkmn_rl_arena.env.turn_type import TurnType
-from dataclasses import dataclass
+from pkmn_rl_arena.env.battle_state import BattleState, BattleStateFactory
+
+import rustboyadvance_py
+
 import os
 from typing import Dict, List, Optional, Tuple
-
-
-@dataclass
-class BattleState:
-    """
-    Represents the current state of the battle
-    NOTE :
-        DO NOTE USE THIS CLASS CONSTRUCTOR.
-        STATES MUST ONLY BE MANUFACTURED THROUGH StateFactory
-    """
-
-    id: int = 0  # unique id for this run
-    step: int = 0  # nb step in this run
-    turn: TurnType = TurnType.CREATE_TEAM  # current battle turntype
-
-    def __eq__(self, other) -> bool:
-        """This is a helper for tests, it doesn't tests the id voluntarly ad it is unique."""
-        return self.step == other.step and self.turn == other.turn
-
-
-class BattleStateFactory:
-    id_gen: int = 0  # unique episode id  generator
-    id: int = 0  # unique id for this run
-
-    def build(self, current_turn=TurnType.CREATE_TEAM, step=0) -> BattleState:
-        if step < 0:
-            raise ValueError(
-                f"Attempting to create a step with negative step, step value must be strictly > 0, got {step}"
-            )
-        id = BattleStateFactory.id_gen
-        BattleStateFactory.id_gen += 1
-        return BattleState(id=id, step=step, turn=current_turn)
-
-    @staticmethod
-    def from_save_path(save_path: str) -> BattleState:
-        """
-        Creates state from save path, assuming save path is of shape :
-        {save_name}_turntype:{turntype.value}_step:{step}_id:{id}.savestate
-        """
-        data = save_path.split(".")[-2].split("_")[-3:]
-
-        turntype = TurnType(int(data[0][data[0].find(":") + 1 :]))
-        step = int(data[1][data[1].find(":") + 1 :])
-        id = int(data[2][data[2].find(":") + 1 :])
-
-        return BattleState(id, step, turntype)
 
 
 class BattleCore:
