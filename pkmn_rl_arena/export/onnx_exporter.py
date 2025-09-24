9703f6ff4e8f88bd62ca9785cd9c15e53a2fbb36
wissammm <wissamboussella@gmail.com>
2025-09-24 19:14:40 +0200

    [chore] full tutorial [feat] name sanitizer

--- pkmn_rl_arena/export/onnx_exporter.py
+++ pkmn_rl_arena/export/onnx_exporter.py
@@ -1,9 +1,11 @@
+from sympy import re
 from .export_core.graph_loader import ONNXGraphLoader
 from .export_core.memory_allocator import MemoryAllocator
 from .export_core.exporter_factory import ExporterFactory
 from .export_core.code_generator import CodeGenerator
 from .passes.pass_manager import PassManager
 from .passes.delete_pass import DeleteQuantizePass
+from .onnx_utils import OnnxUtils
 
 class ONNXExporter:
     def __init__(self, onnx_path):
@@ -12,13 +14,18 @@ class ONNXExporter:
     def export(self, output_dir="gba"):
         # Load the model
         loader = ONNXGraphLoader(self.onnx_path)
+
         model = loader.load_model()
+        OnnxUtils.sanitize_graph_names(model.graph)  
+
         value_info = loader.get_value_info()
         
         # Run optimization passes
         pass_manager = PassManager(model.graph)
         pass_manager.add_pass(DeleteQuantizePass())
         pass_manager.run_passes()
+
+
         optimized_graph = pass_manager.get_optimized_graph()
         
         updated_value_info = self._update_value_info(value_info, optimized_graph)
@@ -37,12 +44,15 @@ class ONNXExporter:
         code_generator = CodeGenerator(exporters, allocator)
         code_generator.generate(output_dir)
 
+    
+    
     def _update_value_info(self, value_info, graph):
         """
         Update value_info to match tensor names in the graph.
         Creates a mapping from quantized to regular tensor names.
         """
         updated_info = dict(value_info)
+
         
         # Find all tensor names in graph
         used_tensors = set()
