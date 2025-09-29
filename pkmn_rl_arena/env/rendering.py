a5f9e1449c41097b17277b02eac05298e257a234
greg-is-kub <greg_is_kub@pm.me>
2025-09-29 17:35:54 +0200

    feat (Rendering): refactored the rendering table to more compact & hold more data

--- pkmn_rl_arena/env/rendering.py
+++ pkmn_rl_arena/env/rendering.py
@@ -19,21 +19,21 @@ from rich.live import Live
 
 TYPES_ID = {
     255: "NONE",
-    0: "NORMAL",
-    1: "FIGHTING",
-    2: "FLYING",
-    3: "POISON",
-    4: "GROUND",
+    0: "NORM",
+    1: "FIGHT",
+    2: "FLY",
+    3: "PSN",
+    4: "GRND",
     5: "ROCK",
     6: "BUG",
     7: "GHOST",
     8: "STEEL",
     9: "MYSTERY",
     10: "FIRE",
-    11: "WATER",
+    11: "WTR",
     12: "GRASS",
-    13: "ELECTRIC",
-    14: "PSYCHIC",
+    13: "ELEK",
+    14: "PSY",
     15: "ICE",
     16: "DRAGON",
     17: "DARK",
@@ -50,8 +50,12 @@ class GameRendering:
 
     def _build_table(self, obs: Observation, reward: Dict[str, float]) -> Table:
         main_table = Table(expand=True, padding=(0, 1))
-        main_table.add_column(f"Player\tReward {reward["player"]}", justify="center", header_style="Red")
-        main_table.add_column(f"Enemy\tReward : {reward["enemy"]}", justify="center", header_style="Blue")
+        main_table.add_column(
+            f"Player\tReward {reward['player']}", justify="center", header_style="Red"
+        )
+        main_table.add_column(
+            f"Enemy\tReward : {reward['enemy']}", justify="center", header_style="Blue"
+        )
 
         agent_tables = {
             a: Table(
@@ -69,37 +73,35 @@ class GameRendering:
         active_pkmn = obs.active_pkmn()
 
         for agent, table in agent_tables.items():
-            table.add_column("Pokémon", justify="left", ratio=4, no_wrap=False)
-            table.add_column("Stats / Moves", justify="left", ratio=5, no_wrap=False)
+            # table.add_column("Pokémon", justify="left", ratio=4, no_wrap=False)
+            # table.add_column("Stats / Moves", justify="left", ratio=5, no_wrap=False)
 
             for i, pkmn in enumerate(np.split(obs.agent(agent), DataSize.PARTY_SIZE)):
                 pkmn_id = pkmn[ObsIdx.RAW_DATA["species"]]
                 if pkmn_id == 0:
                     continue
+                pkmn_table = Table(show_header=False, show_lines=False, show_edge=False)
 
+                ############################
+                # NAME & active indicator
                 is_active = True if i == active_pkmn[agent] else False
                 active_indicator = Text("● ", style="red" if is_active else "grey50")
- 
+
                 name = self.team_factory.get_pkmn_name_from(pkmn_id)
                 types = (
                     int(pkmn[ObsIdx.RAW_DATA["type_1"]]),
                     int(pkmn[ObsIdx.RAW_DATA["type_2"]]),
                 )
+                types_str = "Types : "
                 if types[0] != types[1]:
-                    types = (TYPES_ID[type] for type in types)
+                    types_str += (
+                        f"{str(TYPES_ID[types[0]]):^6} / {str(TYPES_ID[types[1]]):^6}"
+                    )
                 else:
-                    types = (TYPES_ID[types[0]],)
-                types_str = ", ".join(types)
-
-                stats_str = f"ATK : {stats[agent][i][0]}\nDEF : {stats[agent][i][1]}\nSPD : {stats[agent][i][2]}\nSPA : {stats[agent][i][3]}\nSPD : {stats[agent][i][4]}"
-                left_col = Group(
-                    active_indicator + Text(f"{name} LVL: {lvls[agent][i]}", style="bold"),
-                    Text(f"{types_str}"),
-                    Text(stats_str),
-                )
+                    types_str += f"{str(TYPES_ID[types[0]]):^6}"
 
                 ############################
-                # HP bar + moves
+                # HP bar
                 max_hp = int(pkmn[ObsIdx.RAW_DATA["max_HP"]])
                 hp = int(pkmn[ObsIdx.RAW_DATA["HP"]])
                 hp_percent = hp / max_hp
@@ -113,28 +115,69 @@ class GameRendering:
                 hp_bar = ProgressBar(
                     total=max_hp,
                     completed=hp,
-                    width=20,
+                    width=None,
                     complete_style=bar_style,  # <- filled part
                     finished_style=bar_style,  # <- when bar is at 100%
                     style="grey37",  # <- background (empty slots)
                 )
+
+                ############################
+                # Stats
+                stats_txt = Text(
+                    f"Atk : {str(stats[agent][i][0]):<6}Def : {str(stats[agent][i][1]):<6}Spd : {str(stats[agent][i][2]):<6}Sp.Atk : {str(stats[agent][i][3]):<6}Sp.Def : {str(stats[agent][i][4]):<6}"
+                )
+                pkmn_table.add_row(
+                    Group(
+                        active_indicator
+                        + Text(
+                            f"{name:<12}{types_str} Lvl {str(lvls[agent][i]):<5}",
+                            style="bold",
+                        ),
+                        stats_txt,
+                        hp_bar,
+                    )
+                )
+
+                ############################
+                # moves
                 moves_renderable = []
-                for move_offset in range(
+                for mv_offset in range(
                     ObsIdx.RAW_DATA["moves_begin"],
                     ObsIdx.NB_DATA_PKMN,
                     ObsIdx.RAW_DATA["move_slot_stride"],
                 ):
-                    move_name = self.team_factory.get_move_name_from(
-                        pkmn[move_offset + ObsIdx.RAW_DATA["move_id_offset"]]
-                    )
-                    move_type = pkmn[move_offset + ObsIdx.RAW_DATA["type_offset"]]
-                    pp = pkmn[move_offset + ObsIdx.RAW_DATA["pp_offset"]]
+                    mv_id = pkmn[mv_offset + ObsIdx.RAW_DATA["move_id_offset"]]
+                    if mv_id == 0:
+                        continue
+                    mv_name = self.team_factory.get_move_name_from(mv_id)
+                    mv_type = TYPES_ID[pkmn[mv_offset + ObsIdx.RAW_DATA["type_offset"]]]
+                    pp = pkmn[mv_offset + ObsIdx.RAW_DATA["pp_offset"]]
+                    max_pp = self.team_factory.moves.loc[mv_id, "pp"]
+                    pp_str = f"{pp}/{max_pp}"
+                    dmg = pkmn[mv_offset + ObsIdx.RAW_DATA["power_offset"]]
                     moves_renderable.append(
-                        Text(f"\n{move_name}\t{move_type}\tpp: {pp}")
+                        Text(
+                            f"\n{mv_name:<14} {mv_type:<7}Dmg : {str(dmg):<2} pp: {pp_str}"
+                        )
                     )
-                right_col = Group(hp_bar, *moves_renderable)
 
-                table.add_row(left_col, right_col)
+                moves_table = Table(
+                    show_edge=False, show_header=False, show_footer=False
+                )
+
+                match len(moves_renderable):
+                    case 1:
+                        moves_table.add_row(moves_renderable[0], "")
+                    case 2:
+                        moves_table.add_row(moves_renderable[0], moves_renderable[1])
+                    case 3:
+                        moves_table.add_row(moves_renderable[0], moves_renderable[1])
+                        moves_table.add_row(moves_renderable[2], "")
+                    case 4:
+                        moves_table.add_row(moves_renderable[0], moves_renderable[1])
+                        moves_table.add_row(moves_renderable[2], moves_renderable[3])
+                pkmn_table.add_row(moves_table)
+                table.add_row(pkmn_table)
 
         main_table.add_row(agent_tables["player"], agent_tables["enemy"])
 
